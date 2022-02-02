e0a13d6febc6bb37538ba655a668fc693b7b245b
Matheus Lemos <96403590+Ddesistencia@users.noreply.github.com>
2022-02-02 14:10:49 -0300

    Add files via upload

---
+++ RGB GENERATOR/script.js
@@ -0,0 +1,12 @@
+function myColor(){
+    const red = document.getElementById('red').value;
+    const green = document.getElementById('green').value;
+    const blue = document.getElementById('blue').value;
+    const color = 'rgb('+ red +', '+ green +', '+ blue +')';
+    document.body.style.backgroundColor = color;
+    document.getElementById('box').value = color;
+}
+
+    document.getElementById('red').addEventListener('input',myColor);
+    document.getElementById('green').addEventListener('input',myColor);
+    document.getElementById('blue').addEventListener('input',myColor);
